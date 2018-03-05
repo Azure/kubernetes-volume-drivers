3d5421882254b1de869ccdf44d9ed8da2eaf1a3f
andyzhangx <xiazhang@microsoft.com>
2018-03-05 07:33:34 +0000

    update blobfuse-flexvol-installer image

--- flexvolume/blobfuse/deployment/blobfuse-flexvol-installer/install.sh
+++ flexvolume/blobfuse/deployment/blobfuse-flexvol-installer/install.sh
@@ -2,6 +2,7 @@
 
 LOG="/var/log/blobfuse-flexvol-installer.log"
 VER="1.0.0"
+BLOBFUSE_VERSION="v0.2.4"
 target_dir="${TARGET_DIR}"
 echo "blobfuse-flexvol-installer ${VER}" >> $LOG
 
@@ -21,11 +22,12 @@ else
 	version=`echo ${KUBELET_VERSION} | awk -F '.' '{print $1"."$2}'`
 fi
 
-wget -O ${blobfuse_bin_dir}/blobfuse https://raw.githubusercontent.com/andyzhangx/kubernetes-drivers/master/flexvolume/blobfuse/binary/hyperkube-$version/v0.2.4/blobfuse >>$LOG 2>&1
+#copy blobfuse binary
+cp /blobfuse/hyperkube-$version/$BLOBFUSE_VERSION/blobfuse ${blobfuse_bin_dir}/blobfuse
 chmod a+x ${blobfuse_bin_dir}/blobfuse
 
-#download blobfuse script
-wget -O ${blobfuse_vol_dir}/blobfuse https://raw.githubusercontent.com/andyzhangx/kubernetes-drivers/master/flexvolume/blobfuse/blobfuse >>$LOG 2>&1
+#copy blobfuse script
+cp /blobfuse/blobfuse ${blobfuse_vol_dir}/blobfuse
 chmod a+x ${blobfuse_vol_dir}/blobfuse
 
 #https://github.com/kubernetes/kubernetes/issues/17182
