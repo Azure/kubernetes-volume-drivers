a6b2172f19214e20858624f5817568d55de3e064
andyzhangx <xiazhang@microsoft.com>
2019-03-08 13:16:43 +0000

    add smb ci test

---
+++ flexvolume/smb/test/run-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DRIVER="flexvolume/smb/deployment/smb-flexvol-installer/smb"
+MNTPATH="/tmp/smb-mnt"
+
+echo "begin to run smb test ..."
+echo "$DRIVER init test..."
+$DRIVER init
+retcode=$?
+if [ $retcode -gt 0 ]; then
+        exit $retcode
+fi
+
+# to-do: $DRIVER unmount $MNTPATH {JSON-PARAM}
+
+mkdir -p $MNTPATH
+echo "$DRIVER unmount test..."
+$DRIVER unmount $MNTPATH
+if [ $retcode -gt 0 ]; then
+        exit $retcode
+fi
+
+echo "smb test is completed."
