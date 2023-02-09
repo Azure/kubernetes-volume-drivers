4c8278abeace2498fdaae9ec512dbf8f1394e4e9
andyzhangx <xiazhang@microsoft.com>
2023-02-09 08:15:24 +0000

    test: reduce sleep time

--- test/cyc_attach_detach_test.sh
+++ test/cyc_attach_detach_test.sh
@@ -4,6 +4,6 @@ for i in $(seq $4)
 do
     echo "`date` test $i" >> $3
     curl -skSL https://raw.githubusercontent.com/Azure/kubernetes-volume-drivers/master/test/attach_detach_test.sh | bash -s $1 $2 $3 --
-    echo "sleep 10 minutes ..." >> $3
-    sleep 10m
+    echo "sleep 3 minutes ..." >> $3
+    sleep 3m
 done
